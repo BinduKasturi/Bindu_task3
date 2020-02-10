0e533b7cbb7d561f95d4b1eb25a29dc39f60c3c9
BinduKasturi <bindukasturi27@gmail.com>
2020-02-10 22:43:24 +0530

    file

---
+++ EpamTask3/src/calculate/Cal.java
@@ -0,0 +1,19 @@
+package calculate;
+public class Cal {
+	public double add(double a,double b)
+	{
+		return a+b;
+	}
+	public double sub(double a,double b)
+	{
+		return a-b;
+	}
+	public double mul(double a,double b)
+	{
+		return a*b;
+	}
+	public double divide(double a,double b)
+	{
+		return a/b;
+	}
+}
